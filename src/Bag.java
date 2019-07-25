d4a458889c7d209fec7f2838a18dc4ac7f0c5af3
jeewoon.han <jeewoon.han@navercorp.com>
2019-07-25 23:46:50 +0900

    3일차

--- src/Bag.java
+++ src/Bag.java
@@ -12,6 +12,17 @@ public class Bag {
         this.amount = amount;
     }
 
+    public long hold(Ticket ticket) {
+        if(hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
 
     public boolean hasInvitation() {
         return invitation != null;
