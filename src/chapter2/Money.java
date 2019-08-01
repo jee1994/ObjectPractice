b5871f7e7f9eee9ca5aa16ed002d087f4cfa0f23
jeewoon.han <jeewoon.han@navercorp.com>
2019-08-02 00:46:21 +0900

    chapter 2 - 1

---
+++ src/chapter2/Money.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+import java.math.BigDecimal;
+
+public class Money {
+    public static final Money ZERO = Money.wons(0);
+
+    private final BigDecimal amount;
+
+    public static Money wons(long amount) {
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    public static Money wons(double amount) {
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/chapter2/Movie.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy runningTime; ß
+}
